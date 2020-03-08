f279e8036fdb31246f0ee8eb30bae4c39fec6c0c
Rafael Ribeiro <rafael.s.ribeiro@sc.senai.br>
2020-03-08 18:13:04 -0300

    Init Commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        transform.position = target.position + offset;
+        ZoomCamera();
+    }
+
+    void ZoomCamera() {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && GetComponent<Camera>().orthographicSize > 3)
+            GetComponent<Camera>().orthographicSize--;
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && GetComponent<Camera>().orthographicSize < 10)
+            GetComponent<Camera>().orthographicSize++;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Animator animator;
+    public Transform Player;
+    Vector3 playerForward, playerRight;
+    float currentSpeed, horMovement, vertMovement;
+    
+
+    void Start()
+    {        
+        PlayerInit();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        horMovement = Input.GetAxis("Horizontal");
+        vertMovement = Input.GetAxis("Vertical");
+        SetVelocity();
+        PlayerAnime();
+        if (Input.anyKey) {
+            PlayerMove();
+        }        
+    }
+
+    void PlayerMove() {
+                
+        Vector3 direction = new Vector3(horMovement, 0, vertMovement);
+        Vector3 rigthMovement = playerRight * currentSpeed * Time.deltaTime * horMovement;
+        Vector3 upMovement = playerForward * currentSpeed * Time.deltaTime * vertMovement;
+
+        Vector3 heading = Vector3.Normalize(rigthMovement + upMovement);
+
+        transform.forward = heading;
+        transform.position += rigthMovement;
+        transform.position += upMovement;
+    }
+
+    void PlayerAnime() {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
+            animator.SetBool("isWalking", true);
+        } else {
+            animator.SetBool("isWalking", false);
+        }
+    }
+
+    void PlayerInit() {
+        playerForward = Player.forward;
+        playerForward.y = 0;
+        playerForward = Vector3.Normalize(playerForward);
+        playerRight = Quaternion.Euler(new Vector3(0, 90, 0)) * playerForward;
+    }
+
+    void SetVelocity() {
+        if ( horMovement != 0f && vertMovement != 0f)
+            currentSpeed = speed * 0.6f;
+        else     
+            currentSpeed = speed * 1f;
+    }
+
+ 
+}
